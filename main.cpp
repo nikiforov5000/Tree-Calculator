bceee0fd6b8fea32a1632726baf7e72c6ed8bd03
Boris Nikiforov <nikiforov5000@gmail.com>
2021-11-19 11:38:57 +0300

    create class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <vector>
+
+class Token {
+
+};
+
+class Tree {
+	template<typename T>
+	class Node {
+	public:
+		Node* left{};
+		Node* right{};
+		T m_value{};
+
+	};
+
+	Node<char>* root{};
+public:
+
+	void Create(std::vector<Token> tokens) {
+
+	}
+	void Insert() {
+
+	}
+	void Destroy() {
+
+	}
+	void Print() {
+
+	}
+};
 
 int main() {
+	
+	
+	
+
 
 	return 0;
 }
